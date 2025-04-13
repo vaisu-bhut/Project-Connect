f70b6c9ddf0e17025655dc6de4c806344b92fd04
Vasu Vinodbhai Bhut <bhut.v@northeastern.edu>
2025-04-12 21:41:09 -0400

    Backend config

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -15,7 +15,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:8080', 'http://localhost:3000'],
+  origin: ['http://localhost:8080', 'https://connect.vasubhut.com'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
