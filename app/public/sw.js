7d6fa531a0554d1acba70528a7c80f9fb324dd9c
vaisu-bhut <bhut.v@northeastern.edu>
2025-04-25 18:10:45 -0400

    Resolving Errors

--- app/public/sw.js
+++ app/public/sw.js
@@ -16,6 +16,9 @@ self.addEventListener('install', event => {
 });
 
 self.addEventListener('fetch', event => {
+  if (event.request.url.startsWith('chrome-extension://') || event.request.url.startsWith('moz-extension://')) {
+    return;
+  }
   event.respondWith(
     caches.match(event.request)
       .then(response => {
