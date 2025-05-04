99c0192e4e2966c5954100cb2e9398e4f5410e3a
Vasu Vinodbhai Bhut <bhut.v@northeastern.edu>
2025-05-04 03:06:12 -0400

    Update main.tsx

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -3,16 +3,16 @@ import App from './App.tsx'
 import './index.css'
 
 // Register service worker
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
-      .then(registration => {
-        console.log('ServiceWorker registration successful');
-      })
-      .catch(err => {
-        console.log('ServiceWorker registration failed: ', err);
-      });
-  });
-}
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', () => {
+//     navigator.serviceWorker.register('/sw.js')
+//       .then(registration => {
+//         console.log('ServiceWorker registration successful');
+//       })
+//       .catch(err => {
+//         console.log('ServiceWorker registration failed: ', err);
+//       });
+//   });
+// }
 
 createRoot(document.getElementById("root")!).render(<App />);
